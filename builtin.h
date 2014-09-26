41ae5477d2a60dc0398a55cd9b222f0d64029a24
wcy123 <wcy123@gmail.com>
2014-09-26 15:38:57 +0800

    remove undefined symbols

--- builtin.h
+++ builtin.h
@@ -2,11 +2,12 @@
 #define VOBA_MODULE_ID "28b4792c-366b-11e4-aa00-08002796644a"
 #define VOBA_SYMBOL_TABLE(XX)                   \
     XX(get_class)                               \
-    XX(gf_apply)                                \
-    XX(apply)                                   \
     XX(to_string)                               \
     XX(to_i32)                                  \
     XX(print)
+
+//(gf_apply)                              
+//(apply)                                 
 
 #define VOBA_MODULE_NAME "voba_builtin"
 #include <voba/include/module_end.h>
