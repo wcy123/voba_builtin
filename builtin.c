ecea5102e6a1a94f64d0ffda450d2c51ed05a955
wcy123 <wcy123@gmail.com>
2014-10-25 11:08:31 +0800

    support `euqal', the generic function

--- builtin.c
+++ builtin.c
@@ -250,6 +250,7 @@ VOBA_FUNC static voba_value_t plus_string(voba_value_t self, voba_value_t args)
     }
     return voba_make_string(ret);
 }
+static voba_value_t gf_equal = VOBA_UNDEF;
 static voba_value_t gf_gt = VOBA_UNDEF;
 static voba_value_t gf_gt_eq = VOBA_UNDEF;
 static voba_value_t gf_lt = VOBA_UNDEF;
@@ -315,46 +316,65 @@ VOBA_FUNC static voba_value_t lt_eq_int(voba_value_t self, voba_value_t args)
     if(a1<=b1) ret = VOBA_TRUE;
     return ret;
 }
+VOBA_FUNC static voba_value_t equal_int(voba_value_t self, voba_value_t args)
+{
+    voba_value_t ret = VOBA_FALSE;
+    VOBA_DEF_ARG3(a,args,0);
+    VOBA_DEF_ARG3(b,args,1);
+    int64_t a1 = voba_value_int_to_i64(a);
+    int64_t b1 = voba_value_int_to_i64(b);
+    if(a1==b1) ret = VOBA_TRUE;
+    return ret;
+}
 EXEC_ONCE_PROGN{
     gf_gt = voba_make_generic_function();
     gf_gt_eq = voba_make_generic_function();
     gf_lt = voba_make_generic_function();
     gf_lt_eq = voba_make_generic_function();
+    gf_equal = voba_make_generic_function();    
+    VOBA_DEFINE_MODULE_SYMBOL(s__3D_3D, gf_equal);
     VOBA_DEFINE_MODULE_SYMBOL(s__3E, gf_gt);
     VOBA_DEFINE_MODULE_SYMBOL(s__3C, gf_lt);
     VOBA_DEFINE_MODULE_SYMBOL(s__3E_3D, gf_gt_eq);
     VOBA_DEFINE_MODULE_SYMBOL(s__3C_3D, gf_lt_eq);
+
     voba_gf_add_class(gf_gt,voba_cls_i8,voba_make_func(gt_int));
     voba_gf_add_class(gf_lt,voba_cls_i8,voba_make_func(lt_int));
     voba_gf_add_class(gf_gt_eq,voba_cls_i8,voba_make_func(gt_eq_int));
     voba_gf_add_class(gf_lt_eq,voba_cls_i8,voba_make_func(lt_eq_int));
+    voba_gf_add_class(gf_equal,voba_cls_i8,voba_make_func(equal_int));
     
     voba_gf_add_class(gf_gt,voba_cls_i16,voba_make_func(gt_int));
     voba_gf_add_class(gf_lt,voba_cls_i16,voba_make_func(lt_int));
     voba_gf_add_class(gf_gt_eq,voba_cls_i16,voba_make_func(gt_eq_int));
     voba_gf_add_class(gf_lt_eq,voba_cls_i16,voba_make_func(lt_eq_int));
+    voba_gf_add_class(gf_equal,voba_cls_i16,voba_make_func(equal_int));
     
     voba_gf_add_class(gf_gt,voba_cls_i32,voba_make_func(gt_int));
     voba_gf_add_class(gf_lt,voba_cls_i32,voba_make_func(lt_int));
     voba_gf_add_class(gf_gt_eq,voba_cls_i32,voba_make_func(gt_eq_int));
     voba_gf_add_class(gf_lt_eq,voba_cls_i32,voba_make_func(lt_eq_int));
+    voba_gf_add_class(gf_equal,voba_cls_i32,voba_make_func(equal_int));
     
     voba_gf_add_class(gf_gt,voba_cls_u8,voba_make_func(gt_int));
     voba_gf_add_class(gf_lt,voba_cls_u8,voba_make_func(lt_int));
     voba_gf_add_class(gf_gt_eq,voba_cls_u8,voba_make_func(gt_eq_int));
     voba_gf_add_class(gf_lt_eq,voba_cls_u8,voba_make_func(lt_eq_int));
+    voba_gf_add_class(gf_equal,voba_cls_u8,voba_make_func(equal_int));
     
     voba_gf_add_class(gf_gt,voba_cls_u16,voba_make_func(gt_int));
     voba_gf_add_class(gf_lt,voba_cls_u16,voba_make_func(lt_int));
     voba_gf_add_class(gf_gt_eq,voba_cls_u16,voba_make_func(gt_eq_int));
     voba_gf_add_class(gf_lt_eq,voba_cls_u16,voba_make_func(lt_eq_int));
+    voba_gf_add_class(gf_equal,voba_cls_u16,voba_make_func(equal_int));
     
     voba_gf_add_class(gf_gt,voba_cls_u32,voba_make_func(gt_int));
     voba_gf_add_class(gf_lt,voba_cls_u32,voba_make_func(lt_int));
     voba_gf_add_class(gf_gt_eq,voba_cls_u32,voba_make_func(gt_eq_int));
     voba_gf_add_class(gf_lt_eq,voba_cls_u32,voba_make_func(lt_eq_int));
+    voba_gf_add_class(gf_equal,voba_cls_u32,voba_make_func(equal_int));
+    
 }
-
 //
 static voba_value_t gf_match = VOBA_UNDEF;
 VOBA_FUNC static voba_value_t match_single(voba_value_t self, voba_value_t args) 
