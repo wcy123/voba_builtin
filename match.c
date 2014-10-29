62c5c60371c746293b3b101ca205eefd2f3db641
wcy123 <wcy123@gmail.com>
2014-10-29 17:41:38 +0800

    load gf_match as soon as possible.

--- match.c
+++ match.c
@@ -26,6 +26,3 @@ VOBA_FUNC static voba_value_t match_single(voba_value_t self, voba_value_t args)
     }
     return ret;
 }
-EXEC_ONCE_PROGN{
-    gf_match = voba_module_var(VOBA_MODULE_LANG_ID, VOBA_MODULE_LANG_ID,VOBA_MODULE_LANG_MATCH);
-}
--- match.h
+++ match.h
@@ -1,3 +1,6 @@
 #pragma once
 VOBA_FUNC static voba_value_t match_single(voba_value_t self, voba_value_t args);
+EXEC_ONCE_PROGN{
+    gf_match = voba_module_var(VOBA_MODULE_LANG_ID, VOBA_MODULE_LANG_ID,VOBA_MODULE_LANG_MATCH);
+}
 
