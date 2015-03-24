602f5c3538c521f23f541389f5739eb3ce0ab05d
wcy123 <wcy123@gmail.com>
2015-03-24 13:40:39 +0800

    add prelude.h

---
+++ prelude/prelude.h
@@ -0,0 +1,7 @@
+#pragma once
+#define VOBA_MODULE_ID "479098c6-d1e3-11e4-bf65-0800272ae0cc"
+
+#define VOBA_SYMBOL_TABLE(XX)      
+    
+#define VOBA_MODULE_NAME "prelude"
+#include <voba/module_end.h>
